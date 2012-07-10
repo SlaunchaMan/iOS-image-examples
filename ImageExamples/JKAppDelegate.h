36b32c9ceaf5bbca7b613e3f65107d38334ffb81
Jeff Kelley <SlaunchaMan@gmail.com>
2012-07-09 23:11:27 -0400

    Initial Commit

---
+++ ImageExamples/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  ImageExamples
+//
+//  Created by Jeffrey Kelley on 7/9/12.
+//  Copyright (c) 2012 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageExamples/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageExamples
+//
+//  Created by Jeffrey Kelley on 7/9/12.
+//  Copyright (c) 2012 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+	}
+}
